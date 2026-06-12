bd4615f423eb9512001a79c24798acda67736824
John Moya <jmoya@colegiocabodehornos.cl>
2026-06-12 15:42:50 -0400

    fix: newline al final de server.ts

--- server.ts
+++ server.ts
@@ -281,3 +281,4 @@ app.use((err: any, req: express.Request, res: express.Response, next: express.Ne
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`API server running on http://0.0.0.0:${PORT}`);
 });
+
